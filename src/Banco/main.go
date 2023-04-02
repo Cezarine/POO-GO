305bf4d38f63db2b7f0211cdf1cb9f2d394343dc
Cezarine <gcezarines@gmail.com>
2023-04-02 19:46:54 -0300

    Modulo 3 finalizado

--- src/Banco/main.go
+++ src/Banco/main.go
@@ -1,20 +1,20 @@
 package main
 
 import (
+	"banco/contas"
 	"fmt"
-	"C:\Desenvolvimento\Cursos\POO-GO\src\Banco\Conta"
 )
 
 func main() {
 	contaDoGuilherme := contas.ContaCorrente{
-		titular:       "Guilherme",
-		numeroAgencia: 589,
-		numeroConta:   123456,
-		saldo:         500.25}
+		Titular:       "Guilherme",
+		NumeroAgencia: 589,
+		NumeroConta:   123456,
+		Saldo:         500.25}
 
 	contaDoGustavo := contas.ContaCorrente{}
-	contaDoGustavo.titular = "Gustavo"
-	contaDoGustavo.saldo = 20
+	contaDoGustavo.Titular = "Gustavo"
+	contaDoGustavo.Saldo = 20
 
 	fmt.Println(contaDoGustavo)
 	fmt.Println("")
@@ -27,15 +27,15 @@ func main() {
 	fmt.Println(contaDoGuilherme.Sacar(valorDoSaque))
 	fmt.Println("")
 	fmt.Println(contaDoGuilherme.Depositar(valorDoDeposito))
-	fmt.Println(contaDoGuilherme.saldo)
+	fmt.Println(contaDoGuilherme.Saldo)
 	fmt.Println("")
 	status := contaDoGuilherme.Transferir(&contaDoGustavo, valorTransferencia)
 	if status {
 		fmt.Println("Transferencia de ", valorTransferencia, "realizada com sucesso")
 		fmt.Println("")
-		fmt.Println("Conta do Guilherme:", contaDoGuilherme.saldo)
+		fmt.Println("Conta do Guilherme:", contaDoGuilherme.Saldo)
 		fmt.Println("")
-		fmt.Println("Conta do Gustavo:", contaDoGustavo.saldo)
+		fmt.Println("Conta do Gustavo:", contaDoGustavo.Saldo)
 	} else {
 		fmt.Println("Transferencia de ", valorTransferencia, "não foi realizado")
 	}
@@ -46,7 +46,7 @@ func main() {
 
 		var contaDaCris *ContaCorrente
 		contaDaCris = new(ContaCorrente)
-		contaDaCris.titular = "Cris"
+		contaDaCris.Titular = "Cris"
 
 		fmt.Println(*contaDaCris)*/
 }
